95d85c0ca44762cd5f1b31b0820ce4b9b391697d
Gavin Didrichsen <gavin.didrichsen@gmail.com>
2020-06-08 15:00:35 +0100

    Update description
    Signed-off-by: Gavin Didrichsen <gavin.didrichsen@gmail.com>

--- controls/cmake_exists.rb
+++ controls/cmake_exists.rb
@@ -7,7 +7,8 @@ control 'core-plans-cmake-exists' do
   impact 1.0
   title 'Ensure cmake exists'
   desc '
-  '
+  Verify cmake by ensuring /bin/cmake exists'
+  
   plan_installation_directory = command("hab pkg path #{plan_origin}/#{plan_name}")
   describe plan_installation_directory do
     its('exit_status') { should eq 0 }
