e90f4513e042a92ee1a32a2ea7fd282b48f5ce3f
Siraj Rauff <sirajrauff@gmail.com>
2021-06-16 00:48:08 -0400

    Bump from Release Candidate to minor version 3
    Signed-off-by: Siraj Rauff <sirajrauff@gmail.com>

--- plan.sh
+++ plan.sh
@@ -1,13 +1,13 @@
 pkg_name=cmake
 pkg_origin=core
 _base_version=3.20
-pkg_version=${_base_version}.0-rc4
+pkg_version=${_base_version}.3
 pkg_maintainer='The Habitat Maintainers <humans@habitat.sh>'
 pkg_license=('BSD-3-Clause')
 pkg_description="CMake is an open-source, cross-platform family of tools designed to build, test and package software"
 pkg_upstream_url="https://cmake.org/"
 pkg_source="https://cmake.org/files/v${_base_version}/cmake-${pkg_version}.tar.gz"
-pkg_shasum=34cda5a38a627e799658d8bb15982701ca5e542e112d88396d42596a03037c7c
+pkg_shasum=4d008ac3461e271fcfac26a05936f77fc7ab64402156fb371d41284851a651b8
 pkg_deps=(
   core/glibc
   core/gcc-libs
